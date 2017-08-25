7b47ff82f3fe202f969b37b44d7de85e4289bd6d
STEVEN TITUAÑA <steven642010@hotmail.com>
2017-08-25 10:12:35 -0500

    dg

--- FrameworkMVC/view/BienvenidaView.php
+++ FrameworkMVC/view/BienvenidaView.php
@@ -47,11 +47,9 @@
 	
      <p style="text-align: justify;">Saludos Estimados, les comunicamos que hemos procedido con la actualización del estado procesal de los juicios que estaban en un estado procesal diferente a <strong>SUSPENSIÓN O ARCHIVADO</strong>, en un estado procesal de nombre 
      <strong> REVISAR</strong>, favor revisar y actualizar sus juicios que se encuentran en dicho estado a su estado procesal correspondiente que puede ser <strong>SUSPENSIÓN O ARCHIVADO</strong>.</p>
-   <br>
-   <p style="text-align: justify;">Si existen juicios <strong>duplicados o juicios que no pertenescan a su matriz</strong>, actualizarlos a un estado procesal de nombre <strong>S/N</strong> para proceder con su eliminación.</p>
    
    <br>
-    <p style="text-align: justify;"><strong>NOTA: </strong>Todo juicio que no este actualizado a un estado procesal de nombres <strong>SUSPENSIÓN O ARCHIVADO</strong> seran eliminados.</p>
+    <p style="text-align: justify;"><strong>NOTA: </strong>Todo juicio que este en estado procesal de nombre <strong>REVISAR</strong> seran eliminados al terminar el proceso de revisón de la matriz de juicios.</p>
    
      </div> 
             
